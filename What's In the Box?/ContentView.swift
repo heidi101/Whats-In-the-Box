a4965996c2b1af896982e961745ec3bf957fd7a2
heidi101 <heidilou168@gmail.com>
2022-05-25 17:59:09 -0700

    Initial Commit

---
+++ What's In the Box?/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  What's In the Box?
+//
+//  Created by HL on 5/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ What's In the Box?/What_s_In_the_Box_App.swift
@@ -0,0 +1,17 @@
+//
+//  What_s_In_the_Box_App.swift
+//  What's In the Box?
+//
+//  Created by HL on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct What_s_In_the_Box_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
